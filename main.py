b93c3105b78cd7dadb229c783a19c89300554e44
Robin Singh Chouhan <59241835+RobinSinghChouhan@users.noreply.github.com>
2024-11-16 13:20:21 +1100

    version 1 of calculator
    This calculates discount for amount greater than $100.

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+### Make a discount calculator which
+# gives back the discount amount like
+# if its amount more than 100 print discount
+# applied else no discount
+
+amount = 100
+if amount>100:
+    print("discount applied")
+else:
+    print("no discount")
+
+
